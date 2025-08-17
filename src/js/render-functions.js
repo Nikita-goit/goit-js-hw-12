7c1d39301534a1ffd071c6ed708bdf92ed773846
Nikita Mulyar <nikitamulyar228@gmail.com>
2025-08-17 23:35:31 +0300

    Fixed async/await

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,12 +1,16 @@
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 let lightboxInstance;
 
+
+// СТВОРЕННЯ РОЗМІТКИ
 export function createGallery(images) {
   const galleryContainer = document.querySelector('.gallery');
   
   const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, downloads, comments}) => `
-    <a href="${largeImageURL}" class="gallery_item">
+    <a href="${largeImageURL}" class="gallery_item elem-js">
       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
 
 <ul class="stats-container">
@@ -38,20 +42,97 @@ export function createGallery(images) {
     lightboxInstance.refresh();
   }}
 
+  // ОЧИСТИТИ ГАЛЕРЕЮ
   export function clearGallery() {
   const galleryContainer = document.querySelector('.gallery');
   galleryContainer.innerHTML = '';
 }
 
+// ПОКАЗАТИ ЛОАДЕР
 export function showLoader(){
-  const loader = document.getElementById('loader');
-if(loader){
-    loader.style.display = 'inline-block'
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.remove('is-not-active');
+  } else {
+    console.warn('Loader element not found');
+  }
+}
+// ЗХОВАТИ ЛОАДЕР
+export function hideLoader(){
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.add('is-not-active');
+  } else {
+    console.warn('Loader element not found in hideLoader');
+  }
 }
 
+// ПОКАЗАТИ КНОПКУ "MORE PAGE"
+export function showLoadMoreButton(){
+  const button = document.querySelector('.button-js')
+  button.classList.remove('is-not-active')
 }
-export function hideLoader(){
-const loader = document.getElementById('loader');
-if(loader){
-loader.style.display = 'none'}
+
+// СХОВАТИ КНОПКУ "MORE PAGE"
+export function hideLoadMoreButton(){
+  const button = document.querySelector('.button-js')
+  button.classList.add('is-not-active')
 }
+
+// ДОДАТИ ГАЛЕРЕЮ
+
+export function createMoreGallery(images){
+
+
+    const galleryContainer = document.querySelector('.gallery');
+  
+  const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, downloads, comments}) => `
+    <a href="${largeImageURL}" class="gallery_item">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+
+<ul class="stats-container">
+  <li class="stat-item">
+    <span>Likes</span>
+    <span>${likes}</span>
+  </li>
+  <li class="stat-item">
+    <span>Views</span>
+    <span>${views}</span>
+  </li>
+  <li class="stat-item">
+    <span>Comments</span>
+    <span>${comments}</span>
+  </li>
+  <li class="stat-item">
+    <span>Downloads</span>
+    <span>${downloads}</span>
+  </li>
+</ul>
+    </a>
+  `).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup)
+  
+
+  if (!lightboxInstance) {
+    lightboxInstance = new SimpleLightbox('.gallery a');
+  } else {
+    lightboxInstance.refresh();
+  }}
+
+
+export function smoothScroll() {
+  const elements = document.querySelectorAll('.elem-js');
+  const lastElement = elements[elements.length - 1];
+  if (!lastElement) return;
+
+  // Висота останнього елемента
+  const height = lastElement.getBoundingClientRect().height;
+
+  // Прокрутка вниз на цю висоту
+  window.scrollBy({
+    top: height,
+    behavior: 'smooth'
+  });
+}
+
--- src/main.js
+++ src/main.js
@@ -2,32 +2,35 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-// Описаний у документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 
 import { getImagesByQuery } from "./js/pixabay-api";
-import { createGallery } from "./js/render-functions";
+import { createGallery, hideLoadMoreButton, showLoadMoreButton } from "./js/render-functions";
 import { showLoader } from "./js/render-functions";
 import { hideLoader } from "./js/render-functions";
 import { clearGallery } from "./js/render-functions";
+import { createMoreGallery } from "./js/render-functions";
+import { smoothScroll } from "./js/render-functions";
+
 
 const form = document.querySelector('.form')
 const searchText = document.querySelector('[name="search-text"]')
+const buttonLoadMore = document.querySelector('.button-js')
+const per_page = 15;
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   showLoader()
-// });
-// window.addEventListener('load', () => {
-//   hideLoader()
-// });
+let totalHitsValue = ''
+let page = 1;
+let searchTextValue = '';
+
+
 
 
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    searchTextValue = searchText.value.trim()
+    
     console.log(searchText.value)
  if(searchText.value.trim() === ''){
           iziToast.error({
@@ -35,28 +38,76 @@ form.addEventListener('submit', (e)=>{
             })  
           return
         }
-        
-
     clearGallery()
     showLoader()
-
-
-    getImagesByQuery(searchText.value.trim()).then(images => {
-        if (images.length === 0){
+    page = 1;
+    async function getImagesFunction(searchTextValue, page) {
+      try {
+        const {hits, totalHits} = await getImagesByQuery(searchTextValue, page)
+        const totalPages = Math.ceil(totalHits / per_page)
+        if (hits.length === 0){
             iziToast.error({
                 message: 'Sorry, there are no images matching your search query. Please try again!',
             }) 
-            
+            hideLoadMoreButton()
+            return
+            // ПЕРЕВІРКА PAGE
+        } else if (page >= totalPages){
+          iziToast.show({
+          message: 'Were sorry, but youve reached the end of search results.'
+          })
+          hideLoadMoreButton()
+          return
+          }
+        createGallery(hits)
+        showLoadMoreButton()
         }
-        
-        createGallery(images)
-    })
-    .catch(err =>{
+      catch(err){
       iziToast.error({
         message: 'Please try again later'
       })
-    })
-    .finally(()=>{
+      console.error(err)
+    }
+      finally{
       hideLoader()
+    }
+}
+getImagesFunction(searchTextValue, page);
+
+});
+
+  
+
+
+
+buttonLoadMore.addEventListener('click', ()=>{
+
+  
+  hideLoadMoreButton()
+  showLoader()
+  searchTextValue = searchText.value.trim()
+  getImagesByQuery(searchTextValue, ++page)
+  .then(({hits, totalHits}) =>{
+    const totalPages = Math.ceil(totalHits / per_page) 
+    if (page >= totalPages){
+    iziToast.show({
+      message: 'Were sorry, but youve reached the end of search results.'
     })
+    hideLoadMoreButton()
+    return
+  }
+    createMoreGallery(hits)
+    smoothScroll()
+    showLoadMoreButton()
+    
+    
+  })
+  .catch((error)=>{
+  console.error(error)
 })
+.finally(()=>{
+hideLoader()
+})
+})
+
+
